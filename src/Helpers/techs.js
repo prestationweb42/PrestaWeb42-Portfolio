d62f0698cd50edfca6268e9db092ddcd7cd41e5d
danivent23122020 <dani.vent@orange.fr>
2022-11-05 20:22:16 +0100

    commit

---
+++ src/Helpers/techs.js
@@ -0,0 +1,70 @@
+export const techs = [
+    {
+        nom: "React",
+        iconName: "react",
+    },
+    {
+        nom: "Github",
+        iconName: "github",
+    },
+    {
+        nom: "PHP",
+        iconName: "php",
+    },
+    {
+        nom: "Symfony",
+        iconName: "symfony",
+    },
+    {
+        nom: "Javascript",
+        iconName: "javascript",
+    },
+    {
+        nom: "Html5",
+        iconName: "html5",
+    },
+    {
+        nom: "Css3_full",
+        iconName: "css3_full",
+    },
+    {
+        nom: "Bootstrap",
+        iconName: "bootstrap",
+    },
+    {
+        nom: "Sass",
+        iconName: "sass",
+    },
+    {
+        nom: "WordPress",
+        iconName: "wordpress",
+    },
+    {
+        nom: "NPM",
+        iconName: "npm",
+    },
+    {
+        nom: "VisualstudioCode",
+        iconName: "visualstudio",
+    },
+    {
+        nom: "Chrome",
+        iconName: "chrome",
+    },
+    {
+        nom: "Terminal",
+        iconName: "terminal",
+    },
+    {
+        nom: "Code",
+        iconName: "code",
+    },
+    {
+        nom: "Photoshop",
+        iconName: "photoshop",
+    },
+    {
+        nom: "Mysql",
+        iconName: "mysql",
+    },
+];
